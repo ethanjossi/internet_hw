f9798953d9ef1fdc2e4acd4a205031d024c00a14
ethanjossi <jossi.ethan@gmail.com>
2024-10-30 23:35:27 -0500

    Last changes to hw3.

--- Homework3/server.py
+++ Homework3/server.py
@@ -1,28 +1,70 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import urllib
 import json
-import copy
-from utils import filepaths, BID_TEMPLATE, LISTING_PAGE, TABLE_TEMPLATE, escape_html, unescape_url
+from datetime import datetime
 
-# PUT YOUR GLOBAL VARIABLES AND HELPER FUNCTIONS HERE.
 # Load all the listings from the json file
 with open("static/json/listings.json", "r") as file:
     listings = json.load(file)
 
+# PUT YOUR GLOBAL VARIABLES AND HELPER FUNCTIONS HERE.
+LISTING_PAGE = "listing_page.html"
+TABLE_TEMPLATE = """
+<tr class="listingEntry" data-image="{imageURL}" data-description="{description}">
+    <td>
+        <a href="{URL}">{title}</a>
+    </td>
+    <td>{num_bids}</td>
+    <td>${top_bid}</td>
+    <td>{endDate}</td>
+</tr>"""
+BID_TEMPLATE = """
+<div class="bid">
+    <p><span class="bidder">{bidderName}: </span><span class="amount">{bidAmount}</span></p>
+    <p>Comments: {comment}</p>
+</div>"""
+# Dictionary to find page URLs
+filepaths = {
+        "/": "static/html/mainpage.html",
+        "/main": "static/html/mainpage.html",
+        "css": "static/css/main.css",
+        "/gallery": "static/html/listings.html",
+        "/gallery/search": "static/html/listings.html",
+        "/create": "static/html/create.html",
+        "/listing/0": LISTING_PAGE,
+        "/listing/1": LISTING_PAGE,
+        "/listing/2": LISTING_PAGE,
+        "/listing/3": LISTING_PAGE,
+        "/images/main": "static/images/main.jpg",
+        "/images/0": "static/images/0.jpg",
+        "/images/1": "static/images/1.jpeg",
+        "/images/2": "static/images/2.jpeg",
+        "/images/3": "static/images/3.jpg",
+        "/main.css": "static/css/main.css",
+        "/js/bid.js": "static/js/bid.js",
+        "/js/new_listing.js": "static/js/new_listing.js",
+        "/js/table.js": "static/js/table.js"
+    }
 
-def parse_id(id=None):
-    json_data = [listing for listing in listings if listing['id'] == id]
-    return json.dumps(json_data, indent=4)
-    
+def escape_html(str):
+    str = str.replace("&", "&amp;")
+    str = str.replace('"', "&quot;")
+    str = str.replace("'", "&#39;")
+    str = str.replace("<", "&lt;")
+    str = str.replace(">", "&gt;")
 
-def parse_query_category(query=None, category=None):
-    json_data = [listing for listing in listings
-        if (query is None or query.lower() in listing['title'].lower()) and 
-           (category is None or category.lower() == listing['category'].lower())
-    ]
-    return json.dumps(json_data, indent=4)
+    return str
 
-def parse_query_parameters(response):
+# Use this function only after splitting the paramters & anchor from the URL
+def unescape_url(url_str):
+    import urllib.parse
+
+    # NOTE -- this is the only place urllib is allowed on this assignment.
+    return urllib.parse.unquote_plus(url_str)
+
+
+def parse_query_parameters(response) -> dict:
+    if response is None:
+        return {"category": "all", "query": ""}
     # If ? at beginning, remove it
     if response.startswith("?"):
         response = response[1:]
@@ -38,6 +80,106 @@ def parse_query_parameters(response):
         value = unescape_url(value)
         query_dict[key] = value
     return query_dict
+
+
+def render_listing(listing) -> str:
+    title = listing["title"]
+    imageURL = listing["imageURL"]
+    description = listing["description"]
+    category = listing["category"]
+    endDate = listing["endDate"]
+    id = listing["id"]
+    # Format all the bids
+    bids = ""
+    for bid in listing["bids"]:
+        bids += BID_TEMPLATE.format(bidderName=bid["bidderName"], bidAmount=typeset_dollars(bid["bidAmount"]), comment=bid["comment"])
+    if bids == "":
+        bids = "<p>Be the first to bid!</p>"
+    # Write the html
+    with open("static/html/listing_page.html") as f:
+        html = f.read()
+    html = html.format(title=title, id=id, imageURL=imageURL, description=description, category=category, endDate=endDate, bids=bids)
+    return html
+
+def render_gallery(query=None, category=None):
+    with open("static/html/listings.html") as f:
+        html = f.read()
+    # loop over the elements of listings
+    table_entries = ""
+    for entry in listings:
+        if ((query is None) or (query.lower() in entry.get("title").lower())) and ((category is None) or (category == entry.get("category"))):
+            # The entry meets the search parameters, add it to the table entries
+            url = "/listing/{num}".format(num=entry["id"])
+            if entry["bids"] == []:
+                bid_amount = 0
+            else:
+                bid_amount = get_max_bid(entry["id"])
+            table_entries += TABLE_TEMPLATE.format(imageURL=entry["imageURL"], description=entry["description"], URL=url, title=entry["title"], num_bids=len(entry["bids"]), top_bid=bid_amount, endDate=entry["endDate"])
+    # Format the table entries into the listings page
+    if table_entries == "":
+        table_entries = "<p>No listings found</p>"
+    html = html.format(table_entries=table_entries)
+    return html
+
+def add_new_listing(queryParams: dict) -> bool:
+    # Check first that all the entries are there
+    if ("title" not in queryParams) or ("url" not in queryParams) or ("description" not in queryParams) or ("category" not in queryParams) or ("saleDate" not in queryParams):
+        # print(queryParams)
+        return False
+    # Convert sale date and check if sale date is valid
+    year = queryParams.get("saleDate")[0:4]
+    month = queryParams.get("saleDate")[5:7]
+    day = queryParams.get("saleDate")[8:10]
+    sale_date = "{year}-{month}-{day}T14:00:00Z".format(year=year, month=month, day=day)
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    if (datetime(year, month, day) < datetime.now()):
+        return False
+    # Check if category is other, that other is not missing
+    if queryParams.get("category") == "other" and queryParams.get("otherCategory") == "":
+        return False
+    # Add the appropriate listing to the listing dictionary
+    new_listing = {}
+    new_listing["title"] = queryParams.get("title")
+    new_listing["imageURL"] = queryParams.get("url")
+    new_listing["description"] = queryParams.get("description")
+    if queryParams.get("category") == "other":
+        new_listing["category"] = queryParams.get("otherCategory")
+    else:
+        new_listing["category"] = queryParams.get("category")
+    id = listings[-1].get("id") + 1
+    new_listing["id"] = id
+    new_listing["endDate"] = sale_date
+    new_listing["bids"] = []
+    listings.append(new_listing)
+    filepaths["/listing/{}".format(id)] = LISTING_PAGE
+    return True
+
+def add_new_bid(queryParams: dict) -> bool:
+    # Check if all the entries are there
+    if ("name" not in queryParams) or ("amount" not in queryParams) or ("comment" not in queryParams) or ("bidID" not in queryParams):
+        return False
+    # Check that the bid is a number, the listing id exists and that it's larger than the current max bid
+    if (not queryParams.get("bidID").isdigit()):
+        return False
+    id = int(queryParams.get("bidID"))
+    if (get_listing(id) == None) or (int(queryParams.get("amount")) <= get_max_bid(id)):
+        return False
+    # Add the bid to the listing
+    bid = {}
+    bid["bidderName"] = queryParams.get("name")
+    bid["bidAmount"] = int(queryParams.get("amount"))
+    bid["comment"] = queryParams.get("comment")
+    get_listing(id).get("bids").insert(0, bid)
+    return True
+
+def get_max_bid(id: int) -> int:
+    listing = get_listing(id)
+    if not listing.get("bids"): # list is empty
+        return 0
+    max_bid = max(listing.get("bids"), key=lambda x: x["bidAmount"])
+    return max_bid["bidAmount"]
 
 # Provided function -- converts numbers like 42 or 7.347 to "$42.00" or "$7.35"
 def typeset_dollars(number):
@@ -93,18 +235,6 @@ def get_filepath(path: str) -> str:
     return filepath
 
 def server_GET(url: str) -> tuple[str | bytes, str, int]:
-    """
-    url is a *PARTIAL* URL. If the browser requests `http://localhost:4131/contact?name=joe`
-    then the `url` parameter will have the value "/contact?name=joe". (so the schema and
-    authority will not be included, but the full path, any query, and any anchor will be included)
-
-    This function is called each time another program/computer makes a request to this website.
-    The URL represents the requested file.
-
-    This function should return three values (string or bytes, string, int) in a list or tuple. The first is the content to return
-    The second is the content-type. The third is the HTTP Status Code for the response
-    """
-    # YOUR CODE GOES HERE!
     # This gets both the path, anchor, and query in a small amount of code
     # Will still work even if query and anchor aren't in the url
     path = url
@@ -117,9 +247,14 @@ def server_GET(url: str) -> tuple[str | bytes, str, int]:
 
     filepath = get_filepath(path)
     send_data = mime = ""
-    http_status = 200
+    status_code = 200
+    # Individual Listing
+    if filepath == LISTING_PAGE:
+        number = path[9:]
+        send_data = render_listing(get_listing(int(number)))
+        mime = "text/html"
     # Images
-    if filepath.startswith("static/images/"):
+    elif filepath.startswith("static/images/"):
         print(f"requesting image {filepath}")
         with open(filepath, "rb") as img:
             send_data = img.read()
@@ -129,52 +264,73 @@ def server_GET(url: str) -> tuple[str | bytes, str, int]:
         with open(filepath) as file:
             send_data = file.read()
         mime = "text/css"
-    # JS
+    # Listing Page
+    elif filepath == "static/html/listings.html":
+        query_params = parse_query_parameters(query)
+        # Check that category and query exist, if not, add them. 
+        if "category" not in query_params or "query" not in query_params:
+            query_params["category"] = "all"
+            query_params["query"] = ""
+        # Set them to None if they are for any value
+        if query_params["category"] == "all":
+            query_params["category"] = None
+        if query_params["query"] == "":
+            query_params["query"] = None
+        send_data = render_gallery(query=query_params["query"], category=query_params["category"])
+        mime = "text/html"
+    # Javascript
     elif filepath.startswith("static/js"):
         with open(filepath) as file:
             send_data = file.read()
         mime = "text/javascript"
-    # JSON
-    elif filepath.startswith("static/json"):
-        query_params = parse_query_parameters(query)
-        # If gallery
-        if "category" in query_params and "query" in query_params:
-            if query_params["category"] == "all":
-                query_params["category"] = None
-            if query_params["query"] == "":
-                query_params["query"] = None
-            send_data = parse_query_category(query_params["query"], query_params["category"])
-        # else listing
-        else:
-            send_data = parse_id(query_params["id"])
-        mime = "application/json"
     # All other pages
     else:
         with open(filepath) as file:
             send_data = file.read()
         mime = "text/html"
-    print(f"webpath: {path}     filepath: {filepath}      mime: {mime}")
+    print(f"path: {path} filepath: {filepath}")
+    if filepath == "static/html/404.html":
+        status_code = 404
     # ADD IN CHARSET
-    return send_data, mime, http_status
+    return send_data, mime, status_code
 
 
 def server_POST(url: str, body: str) -> tuple[str | bytes, str, int]:
-    """
-    url is a *PARTIAL* URL. If the browser requests `http://localhost:4131/contact?name=joe`
-    then the `url` parameter will have the value "/contact?name=joe". (so the schema and
-    authority will not be included, but the full path, any query, and any anchor will be included)
+    path = url
+    query = anchor = None
+    if '#' in url:
+        path, anchor = url.split("#")
+    if '?' in url:
+        path, query = path.split("?")
+    print(f"filepath: {path},  query: {query}, anchor: {anchor}")
+    print(f"BODY: {body}")
 
-    This function is called each time another program/computer makes a POST request to this website.
+    send_data = mime = ""
+    status_code = 200
 
-    This function should return three values (string or bytes, string, int) in a list or tuple. The first is the content to return
-    The second is the content-type. The third is the HTTP Status Code for the response
-    """
-    pass
+    if path == "/create":
+        query_params = parse_query_parameters(body)
+        result = add_new_listing(query_params)
+        if result:
+            status_code = 201
+            with open("static/html/create_success.html") as file:
+                send_data = file.read()
+        else:
+            status_code = 400
+            with open("static/html/create_fail.html") as file:
+                send_data = file.read()
+        mime = "text/html"
+    elif path == "/place_bid":
+        query_params = parse_query_parameters(body)
+        result = add_new_bid(query_params)
+        status_code = 201 if result else 400
+        send_data = render_listing(get_listing(int(query_params.get("bidID"))))
+        mime = "text/html"
+    
+    return send_data, mime, status_code
 
 
 # You shouldn't need to change content below this. It would be best if you just left it alone.
-
-
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # Read the content-length header sent by the BROWSER
--- Homework3/static/js/table.js
+++ Homework3/static/js/table.js
@@ -1,32 +1,64 @@
-async function loadListings(event) {
-    // event.preventDefault()
-    try {
-        const query = document.getElementById("query").value;
-        const category = document.getElementById("category").value;
-        const fetch_url = `/json/listings.json?query=${query}&category=${category}`;
-        const data = await fetch(fetch_url);
-        const listings = await data.json();
-        const table = document.getElementById("table_entries");
-        listings.forEach(element => {
-            const entry = document.createElement("tr");
-            entry.classList.add("listingEntry");
-            entry.innerHTML = `
-                <td>
-                    <a href="/listing/${element.id}">${element.title}</a>
-                </td>
-                <td>${element.bids.length}</td>
-                <td>${element.bids[0].bidAmount}</td>
-                <td>${element.endDate}</td>`;
-            table.appendChild(entry)
-        });
-    } catch (error) {
-        console.error("There was an error fetching the listings", error);
+function calc_time_difference(endTime) {
+    const now = new Date();
+    let difference = Math.floor((endTime - now) / 1000);
+    // Check if the auction ended
+    if (difference <= 0) {
+        return "Auction Ended";
+    }
+    const days = Math.floor(difference/86400);
+    difference = difference - days*86400;
+    const hours = Math.floor(difference/3600);
+    difference = difference - hours*3600;
+    const minutes = Math.floor(difference/60);
+    difference = difference - minutes*60;
+    const seconds = difference;
+
+    return `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
+}
+
+function update_auction_times() {
+    const table_rows = document.querySelectorAll(".listingEntry");
+    for (let i = 0; i < table_rows.length; i++) {
+        const timeCell = table_rows[i].cells[3];
+        const endTime = new Date(auction_entries[i]);
+        timeCell.textContent = calc_time_difference(endTime);
     }
 }
 
-// Add an event listener for the search button
-// create a search function that parses the search parameters and passes them
-// to the loadListings() function which then reloads the page. 
+function setup_event_listeners() {
+    const table_rows = document.querySelectorAll(".listingEntry");
+    const image = document.getElementById("imageURL");
+    const description = document.getElementById("description");
+    table_rows.forEach(entry => {
+        entry.addEventListener('mouseenter', () => {
+            const imageURL = entry.getAttribute("data-image");
+            const descriptionText = entry.getAttribute("data-description");
+            image.src = imageURL;
+            image.hidden = false;
+            description.textContent = descriptionText;
+            description.hidden = false;
+        })
+    });
 
-loadListings();
-document.getElementById("searchListings").addEventListener("submit", loadListings);
+    // This is for the border for the div
+    const galleryDiv = document.querySelector('.gallery');
+    const previewDiv = document.querySelector('.preview');
+    galleryDiv.addEventListener('mouseenter', () => {
+        previewDiv.style.border = '10px solid black'; 
+        previewDiv.style.borderRadius = '1em';
+    });
+}
+
+// Set up the event listeners for the preview
+setup_event_listeners();
+
+// Store all the times initially
+let auction_entries = []
+const auction_end_times = document.querySelectorAll(".listingEntry");
+auction_end_times.forEach(entry => {
+    const timeCell = entry.cells[3];
+    auction_entries.push(timeCell.textContent);
+})
+
+// Interval that updates the auction times every second
+setInterval(update_auction_times, 1000);
